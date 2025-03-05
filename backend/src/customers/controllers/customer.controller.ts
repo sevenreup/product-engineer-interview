e7076ff984ad0b9e9837d78d1f455629cbfc682a
Christopher Seven Phiri <chris7phiri@gmail.com>
2025-03-05 10:55:18 +0200

    Update customer.controller.ts

--- backend/src/customers/controllers/customer.controller.ts
+++ backend/src/customers/controllers/customer.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 import { CustomerService } from '../service/customer.service';
+import { CreateCustomerDto } from 'libs/database/dto/create-customer.dto';
 
 @Controller('customers')
 export class CustomerController {
@@ -10,5 +11,11 @@ export class CustomerController {
   @UseGuards(JwtAuthGuard)
   findAll() {
     return this.customerService.getAllCustomers();
+  }
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  create(@Body() dto: CreateCustomerDto) {
+    return this.customerService.createCustomer(dto);
   }
 }
